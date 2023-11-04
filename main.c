f66e1130e387446fd1a26b017237b4001db7a4d7
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-11-04 21:10:09 +0900

    Update main.c 03-2

--- main.c
+++ main.c
@@ -5,17 +5,21 @@
 int main(int argc, char *argv[])
 {
     int i;
-    int grade[N_STUDENT];
-    int score[N_STUDENT];
+    int a[N_STUDENT] = {1, 2, 3, 4, 5};
+    int b[N_STUDENT] = {1, 2, 3, 4, 5};
+    int flag = 0;
 
     for(i = 0; i < N_STUDENT; i++)
-        grade[i] = rand() % 100 + 1;
+    {
+        if(a[i] =! b[i])
+        {
+            printf("array a and b are not same\n");
+            flag = 1;
+        }
+    }
 
-    for(i = 0; i < N_STUDENT; i++)
-        score[i] = grade[i];
-
-    for(i = 0; i < N_STUDENT; i++)
-        printf("score[%d] = %d (grade: %i)\n", i, score[i], grade[i]);
+    if(flag == 0)
+        printf("array a and b are same\n");
     
     return 0;
 }
