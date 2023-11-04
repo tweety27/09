e052e80c55731746504d43252052ce364567e624
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-11-04 21:14:45 +0900

    Update main.c 04

--- main.c
+++ main.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N_STUDENT 5
-int main(int argc, char *argv[])
+#define SIZE 4
+
+void square_array(int a[], int size)
 {
     int i;
-    int a[N_STUDENT] = {1, 2, 3, 4, 5};
-    int b[N_STUDENT] = {1, 2, 3, 4, 5};
-    int flag = 0;
 
-    for(i = 0; i < N_STUDENT; i++)
-    {
-        if(a[i] =! b[i])
-        {
-            printf("array a and b are not same\n");
-            flag = 1;
-        }
-    }
+    for (i = 0; i < SIZE; i++)
+        a[i] = a[i] * a[i];
+}
 
-    if(flag == 0)
-        printf("array a and b are same\n");
-    
+void print_array(int a[], int size)
+{
+    int i;
+
+    for(i = 0; i < size; i++)
+        printf("%3d", a[i]);
+    printf("\n");
+}
+int main(int argc, char *argv[])
+{
+    int list[SIZE] = {1, 2, 3, 4};
+
+    print_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
+
+    system("PAUSE");
     return 0;
 }
